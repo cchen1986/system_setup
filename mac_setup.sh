103e6bcc1d8fa60cac63822544d25aa54b8f2ace
Chen Chen <chenchen@chens-mbp.lan>
2020-07-28 23:56:04 -0700

    init

--- mac_setup.sh
+++ mac_setup.sh
@@ -5,6 +5,11 @@ echo "Setting up Mac ..."
 CURRENT_PATH=`pwd`
 echo "Current path: $CURRENT_PATH"
 
+cp inputrc $HOME/.inputrc
+source $HOME/.inputrc
+
+cp vimrc $HOME/.vimrc
+
 # Install homebrew.
 echo "Installing Homebrew ..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
@@ -18,23 +23,16 @@ export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 
 # Jupyter notebook
 echo "Installing Jupyter Notebook ..."
-pip install jupyterlab
+pip3 install --user jupyterlab
+
+brew cask install iterm2
 
 # Oh-my-zsh
-brew install zsh
 echo "Installing oh-my-zsh ..."
+brew install zsh zsh-completions
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+source $HOME/.zshrc
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
 cp zshrc $HOME/.zshrc
 source $HOME/.zshrc
-cd $HOME/Downloads
-git clone https://github.com/powerline/fonts.git --depth=1
-cd fonts
-./install.sh
-cd ..
-rm -rf fonts
-cd $CURRENT_PATH
-
-cp inputrc $HOME/.inputrc
-source $HOME/.inputrc
-
-cp vimrc $HOME/.vimrc
--- zshrc
+++ zshrc
@@ -8,7 +8,7 @@ export ZSH="$HOME/.oh-my-zsh"
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME="agnoster"
+ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
@@ -97,6 +97,9 @@ source $ZSH/oh-my-zsh.sh
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
+
+export PATH="$PATH:$HOME/bin:/usr/local/bin:$HOME/.local/bin"
+export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 
 # python
 alias python="python3"
